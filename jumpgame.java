f281a8da6f2bab790da1671ae21669cbd4c6a779
Anmol Prashar <114831229+Anmol1630@users.noreply.github.com>
2025-02-02 21:32:37 +0530

    jumpgame.java

--- jumpgame.java
+++ jumpgame.java
@@ -1,35 +1,13 @@
-package exp2;
-import java.util.Scanner;
-public class JumpGame {
-    public static int jump(int[] nums) {
-        int jumps = 0;
-        int currentEnd = 0;
-        int farthest = 0;
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            farthest = Math.max(farthest, i + nums[i]); 
-
-            if (i == currentEnd) {
-                jumps++;
-                currentEnd = farthest;
-            }
-        }
-        return jumps;
-    }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter array size: ");
-        int n = scanner.nextInt();
-        
-        int[] nums = new int[n];
-        System.out.println("Enter array elements:");
-        for (int i = 0; i < n; i++) {
-            nums[i] = scanner.nextInt();
-        }
-        int result = jump(nums);
-        System.out.println("Minimum jumps needed: " + result);
-
-        scanner.close();
+class Solution {
+    public int jump(int[] nums) {
+      int jumps = 0, currEnd = 0, currFarthest = 0;
+      for(int i = 0; i < nums.length - 1; i ++) {
+          currFarthest = Math.max(currFarthest, i + nums[i]);
+          if(i == currEnd) {
+              currEnd = currFarthest;
+              jumps ++;
+          }
+      }  
+      return jumps;
     }
 }
