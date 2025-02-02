1bda317c9645f4db049c5999653e57864bbbb662
Anmol Prashar <114831229+Anmol1630@users.noreply.github.com>
2025-02-02 21:37:14 +0530

    simplifypath.java

--- simplifypath.java
+++ simplifypath.java
@@ -1,34 +1,22 @@
-package exp2;
-import java.util.*;
-public class SimplifyPath {
-    public static String simplifyPath(String path) {
+class Solution {
+    public String simplifyPath(String path) {
         Stack<String> stack = new Stack<>();
-        String[] parts = path.split("/");
-
-        for (String part : parts) {
-            if (part.equals("..")) {
+        String[] directories = path.split("/");
+        for (String dir : directories) {
+            if (dir.equals(".") || dir.isEmpty()) {
+                continue;
+            } else if (dir.equals("..")) {
                 if (!stack.isEmpty()) {
-                    stack.pop(); 
+                    stack.pop();
                 }
-            } else if (!part.equals("") && !part.equals(".")) {
-                stack.push(part); 
+            } else {
+                stack.push(dir);
             }
         }
-
         StringBuilder result = new StringBuilder();
         for (String dir : stack) {
             result.append("/").append(dir);
         }
-        return result.length() == 0 ? "/" : result.toString();
-    }
-        public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter absolute Unix path: ");
-        String path = scanner.nextLine();
-
-        String result = simplifyPath(path);
-        System.out.println("Simplified Path:" + result);
-
-        scanner.close();
+        return result.length() > 0 ? result.toString() : "/";
     }
 }
